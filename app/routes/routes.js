fc27e43406936a282cfaea7c64e174f74ed5b277
snatera15 <snatera09@gmail.com>
2015-07-08 20:40:46 -0400

    put routes in correct folder

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,38 +1,125 @@
-/*// app/routes.js
+// app/routes.js
 
 // grab the Data model we just created
-var Data = require('./models/data');
 
-    module.exports = function(app) {
 
-        // server routes ===========================================================
-        // handle things like api calls
-        // authentication routes
 
-        // sample api route
-        app.get('/api/data', function(req, res) {
-            // use mongoose to get all data in the database
-            Data.find(function(err, data) {
+var express = require('express');
+var router = express.Router();       // get an instance of the express Router
 
-                // if there is an error retrieving, send the error. 
-                                // nothing after res.send(err) will execute
+
+
+module.exports = function(app) {
+
+    // ROUTES FOR OUR API
+    // =============================================================================       
+
+
+    // all of our routes will be prefixed with /api
+    app.use('/api', router);
+
+
+
+    // middleware to use for all requests
+    router.use(function(request, response, next) {
+        // do logging
+        
+        //console.log('Something is happening in server.js');
+        next(); // make sure we go to the next routes and don't stop here
+    });
+
+
+    // test route to make sure everything is working 
+    // (accessed at GET http://localhost:8080/api)
+    router.get('/', function(request, response) {
+        response.json({ 
+            message: 'hooray! welcome to our api!' 
+        });   
+    });
+
+    // more routes for our API will happen here
+
+
+    // on routes that end in /posts
+    // ----------------------------------------------------
+    router.route('/posts')
+
+        // create a post (accessed at POST http://localhost:8080/api/posts)
+        .post(function(request, response) {
+            
+            var post = new Post();                      // create a new instance of the Post model
+            post.title = request.body.title;    // set the posts title (comes from the request)
+            post.body  = request.body.body;
+
+            // save the post and check for errors
+            post.save(function(err) {
                 if (err)
-                    res.send(err);
+                    response.send(err);
 
-                res.json(data); // return all data in JSON format
+                response.json({ message: 'Post created!' });
+            });
+        })
+
+        // get all the posts (accessed at GET http://localhost:8080/api/posts)
+        .get(function(req, response) {
+
+            Post.find(function(err, posts) {
+                if (err)
+                    response.send(err);
+
+                response.json(posts);
             });
         });
 
-        // route to handle creating goes here (app.post)
 
 
-        // route to handle delete goes here (app.delete)
+    // on routes that end in /posts/:post_id
+    // ----------------------------------------------------
+
+    router.route('/posts/:post_id')
+
+        // get the post with that id (accessed at GET http://localhost:8080/api/posts/:post_id)
+        .get(function(request, response) {
+            Post.findById(request.params.post_id, function(err, post) {
+                if (err) response.send(err);
+                response.json(post);
+            });
+        })
+
+            // update the post with that id (accessed at PUT http://localhost:8080/api/posts/:post_id)
+        .put(function(request, response) {
+
+            // find the post we want
+            Post.findById(request.params.post_id, function(err, post) {
 
 
-        // frontend routes =========================================================
-        // route to handle all angular requests
-        app.get('*', function(req, res) {
-            res.sendfile('./public/index.html'); // load our public/index.html file
-        });
+                    if (err) response.send(err);
 
-    };*/
+                    // update post info
+                    post.title = request.body.title;
+                    post.body = request.body.body;
+
+                    // save the post
+                    post.save(function(err) {
+                        if (err) response.send(err);
+                        response.json({ message: "Post has been updated!" });
+                    });
+            });
+        })
+            
+        // delete the post with this id (accessed at DELETE http://localhost:8080/api/posts/:post_id)
+        .delete(function(req, res) {
+            Post.remove({
+                _id: req.params.post_id
+            }, function(err, post) {
+                if (err) res.send(err);
+                res.json({ message: 'Successfully deleted' });
+            });
+        });  
+
+
+
+};
+
+
+
--- server.js
+++ server.js
@@ -11,11 +11,13 @@ var app			   = express();
 var bodyParser	   = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose       = require('mongoose');
+var apiEndpoints     = require('./app/routes/routes')(app);
 
 
 
 // MAILER =====================================
 var nodemailer     = require('nodemailer');
+
 //**** Turn off security (below) or use OAUTH2 
 // https://www.google.com/settings/security/lesssecureapps ****//
 
@@ -79,112 +81,7 @@ var port = process.env.PORT || 8080;
 
 
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();          // get an instance of the express Router
 
-
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
-
-// middleware to use for all requests
-router.use(function(request, response, next) {
-    // do logging
-    
-    //console.log('Something is happening in server.js');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(request, response) {
-    response.json({ 
-        message: 'hooray! welcome to our api!' 
-    });   
-});
-
-// more routes for our API will happen here
-
-
-// on routes that end in /posts
-// ----------------------------------------------------
-router.route('/posts')
-
-    // create a post (accessed at POST http://localhost:8080/api/posts)
-    .post(function(request, response) {
-        
-        var post = new Post();      				// create a new instance of the Post model
-        post.title = request.body.title;  	// set the posts title (comes from the request)
-        post.body  = request.body.body;
-
-        // save the post and check for errors
-        post.save(function(err) {
-            if (err)
-                response.send(err);
-
-            response.json({ message: 'Post created!' });
-        });
-        
-    })
-
-    // get all the posts (accessed at GET http://localhost:8080/api/posts)
-    .get(function(req, response) {
-
-        Post.find(function(err, posts) {
-            if (err)
-                response.send(err);
-
-            response.json(posts);
-        });
-    });
-
-
-
-// on routes that end in /posts/:post_id
-// ----------------------------------------------------
-
-router.route('/posts/:post_id')
-
-	// get the post with that id (accessed at GET http://localhost:8080/api/posts/:post_id)
-	.get(function(request, response) {
-			Post.findById(request.params.post_id, function(err, post) {
-				if (err) response.send(err);
-				response.json(post);
-			});
-	})
-
-		// update the post with that id (accessed at PUT http://localhost:8080/api/posts/:post_id)
-    .put(function(request, response) {
-
-    		// find the post we want
-    		Post.findById(request.params.post_id, function(err, post) {
-
-
-	    			if (err) response.send(err);
-
-	    			// update post info
-	    			post.title = request.body.title;
-	    			post.body = request.body.body;
-
-	    			// save the post
-	    			post.save(function(err) {
-	    				if (err) response.send(err);
-	    				response.json({ message: "Post has been updated!" });
-	    			});
-    		});
-    })
-        
-    // delete the post with this id (accessed at DELETE http://localhost:8080/api/posts/:post_id)
-    .delete(function(req, res) {
-        Post.remove({
-            _id: req.params.post_id
-        }, function(err, post) {
-            if (err) res.send(err);
-            res.json({ message: 'Successfully deleted' });
-        });
-    });  
 
 
 
